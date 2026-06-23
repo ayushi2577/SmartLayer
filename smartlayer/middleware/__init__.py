b84d87cfe728c8231e8d62808dac086f2075c9b9
ayushi2577 <ayushi.agrawal_cs.h25@gla.ac.in>
2026-06-23 09:18:35 +0530

    name change and init

--- smartlayer/middleware/__init__.py
+++ smartlayer/middleware/__init__.py
@@ -3,9 +3,4 @@ from .ai_request_validator import AIRequestValidator
 from .rate_limiter import RateLimiter
 from .watch_log import WatchLog
 
-__all__ = [
-    'ai_anomaly_detector',
-    'ai_request_validator',
-    'RateLimiter',
-    'watch_log',
-]
+__all__ = ['AIAnomalyDetector', 'AIRequestValidator', 'RateLimiter', 'WatchLog']
